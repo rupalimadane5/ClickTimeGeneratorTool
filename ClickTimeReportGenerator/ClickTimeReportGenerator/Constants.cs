8ae59caba0f472e01f0b1102573d5f1016153ece
rmadane <rmadane@legalzoom.com>
2019-06-07 00:11:09 +0530

    Removed keys

--- ClickTimeReportGenerator/ClickTimeReportGenerator/Constants.cs
+++ ClickTimeReportGenerator/ClickTimeReportGenerator/Constants.cs
@@ -9,10 +9,10 @@
         #endregion
 
         #region Appsettings
-        //public const string Token = "YOnW48KdUemifVA9aTH5e9fx1hBKymKa25xiTnv-BXlS3o7gftrBSw2";//Rupali
-        public const string Token = "cnSzAqrjvcQ1_hwuUAp2hcgaYGiPF-v2cZ1hkhBa7Uiy01Kq9lZViQ2";//Nikhil
-        public const string LZDivisionId = "4aqE7xoEXgW0wN4BGl98OUw2";
-        public const string TimesheetApproverID = "4ul5rVj-ogGXmw61jUGCDWA2";
+        //public const string Token = "";
+        public const string Token = "";
+        public const string LZDivisionId = "";
+        public const string TimesheetApproverID = "";
         #endregion
     }
 }
